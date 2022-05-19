eda67826723bfa5693c60707b9f4685b724a21a4
Bountysupport <101136466+Bountysupport@users.noreply.github.com>
2022-05-19 13:53:06 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$('.number').keyup(function() {
+    var foo = $(this).val().split("-").join(""); // remove hyphens
+    if (foo.length > 0) {
+      foo = foo.match(new RegExp('.{1,4}', 'g')).join("-");
+    }
+    $(this).val(foo);
+  });
+
+  $('.date').keyup(function() {
+    var foo = $(this).val().split("-").join(""); // remove hyphens
+    if (foo.length > 0) {
+      foo = foo.match(new RegExp('.{1,2}', 'g')).join("/");
+    }
+    $(this).val(foo);
+  });
+
